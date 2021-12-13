8e896346de0734bdb9c63fd01a8fa490b54d3a2d
Shadman Ali <shadman005@gmail.com>
2021-12-13 14:00:47 +0530

    mui AppBar addded

--- src/components/AppNavigator.js
+++ src/components/AppNavigator.js
@@ -1,10 +1,33 @@
+import { AppBar, Toolbar, Typography } from '@mui/material'
 import React from 'react'
+import { makeStyles } from '@mui/styles';
+import { Link } from 'react-router-dom'
+
+const useStyles = makeStyles((theme) => ({
+    appBar: {
+        backgroundColor: 'black !important',
+    },
+    link: {
+        textDecoration: 'none',
+    },
+    title: {
+        cursor:'pointer',
+        color: 'white',
+    }
+}))
 
 function AppNavigator() {
+
+    const classes = useStyles()
+
     return (
-        <div>
-            <p>AppNavigator</p>
-        </div>
+        <AppBar className={classes.appBar} position="fixed" variant="h6">
+            <Toolbar>
+                <Link to="/" className={classes.link}>
+                    <Typography className={classes.title}>Pokedex</Typography>
+                </Link>
+            </Toolbar>
+        </AppBar>
     )
 }
 
