551ef795d4bf17b383ee74048884c1001e608ca0
Shadman Ali <shadman005@gmail.com>
2021-12-13 13:25:08 +0530

    App Navigator Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes ,Route } from 'react-router-dom'
+import AppNavigator from './components/AppNavigator'
 import Pokedex from './pages/Pokedex'
 
 function App() {
   return (
     <Router>
+      <AppNavigator />
       <Routes>
         <Route path="/" element={<Pokedex />} />
       </Routes>
---
+++ src/components/AppNavigator.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AppNavigator() {
+    return (
+        <div>
+            <p>AppNavigator</p>
+        </div>
+    )
+}
+
+export default AppNavigator
