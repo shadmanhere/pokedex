9054f58509d1e8ae9b08f85d73a6d4b23df0106f
Shadman Ali <shadman005@gmail.com>
2021-12-18 10:16:10 +0530

    route for pokemon details set

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Routes ,Route } from 'react-router-dom'
 import AppNavigator from './components/AppNavigator'
 import Pokedex from './pages/Pokedex'
+import PokemonDetails from './pages/PokemonDetails'
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <AppNavigator />
       <Routes>
         <Route path="/" element={<Pokedex />} />
-        <Route path="/pokemon/:id" element={<Pokedex />} />
+        <Route path="/pokemon/:id" element={<PokemonDetails />} />
       </Routes>
     </Router>
   )
