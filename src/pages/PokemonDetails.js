b1aed7aed63d292d9c4b59402eb1693a4dc78138
Shadman Ali <shadman005@gmail.com>
2021-12-22 12:20:09 +0530

    pokemondetails recieves data

--- src/pages/PokemonDetails.js
+++ src/pages/PokemonDetails.js
@@ -1,11 +1,23 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { POKEMON_API_URL } from "../config";
+import { useParams } from "react-router-dom";
 
-export default class PokemonDetails extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+export default function PokemonDetails() {
+  const params = useParams();
+  const [pokemon, setPokemon] = useState(null);
+
+  useEffect(() => {
+    const id = params.id;
+    axios.get(POKEMON_API_URL + "/" + id).then((response) => {
+      if (response.status >= 200 && response.status < 300) {
+        setPokemon(response.data);
+      }
+    });
+  });
+  return (
+    <div>
+      <h1 style={{ marginTop: "200px" }}>PokeDetails</h1>
+    </div>
+  );
 }
