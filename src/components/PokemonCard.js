c2f508506f74e5d1ab130da6475bc48298229da8
Shadman Ali <shadman005@gmail.com>
2021-12-22 10:58:46 +0530

    link added to card and link class addedto link

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,41 +1,44 @@
-import { Card, CardContent, CardMedia, Grid, Typography } from '@mui/material'
-import React from 'react'
-import { makeStyles } from '@mui/styles';
-import { Link } from 'react-router-dom';
+import { Card, CardContent, CardMedia, Grid, Typography } from "@mui/material";
+import React from "react";
+import { makeStyles } from "@mui/styles";
+import { Link } from "react-router-dom";
 
-const useStyles = makeStyles(theme => ({
-    card: {
-        cursor:"pointer",
-        backgroundColor: "black !important",
-        color:"white !important",
-        "&:hover": {
-            backgroundColor: "rgb(90,90,90) !important"
-        }
+const useStyles = makeStyles((theme) => ({
+  card: {
+    cursor: "pointer",
+    backgroundColor: "black !important",
+    color: "white !important",
+    "&:hover": {
+      backgroundColor: "rgb(90,90,90) !important",
     },
-    cardMedia: {
-        margin:"auto",
-        width: 130,
-        height: 130
-    },
-    cardContent:{
-        textAlign: 'center'   
-    }
-}))
+  },
+  cardMedia: {
+    margin: "auto",
+    width: 130,
+    height: 130,
+  },
+  cardContent: {
+    textAlign: "center",
+  },
+  link:{
+      textDecoration: "none"
+  }
+}));
 
 export default function PokemonCard(props) {
-    const classes = useStyles()
-    const { pokemon, image } = props
-    const { id, name } = pokemon
-    return (
-            <Grid item xs={12} sm={2} >
-                <Card className={classes.card}>
-                    <CardMedia className={classes.cardMedia} image={image}></CardMedia>
-                    <CardContent className={classes.cardContent}>
-                        <Typography>
-                            {name}
-                        </Typography>
-                    </CardContent>
-                </Card>
-            </Grid>
-    )
+  const classes = useStyles();
+  const { pokemon, image } = props;
+  const { id, name } = pokemon;
+  return (
+    <Grid item xs={12} sm={2}>
+      <Link to={"/pokemon/"+id} className={classes.link}>
+        <Card className={classes.card}>
+          <CardMedia className={classes.cardMedia} image={image}></CardMedia>
+          <CardContent className={classes.cardContent}>
+            <Typography>{name}</Typography>
+          </CardContent>
+        </Card>
+      </Link>
+    </Grid>
+  );
 }
