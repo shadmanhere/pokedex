ffe532d45e8f92f8bbb9a118d866e084e1cc6252
Shadman Ali <shadman005@gmail.com>
2021-12-25 10:23:55 +0530

    action and reducer implemented

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,7 +1,31 @@
+import { TOGGLE_FAVOURITE } from "./action";
+
 const initialData = {
-  favorites: [],
+  favourites: [],
 };
 
-const pokemonReducer = () => (state = initialData, action) => {}
+const pokemonReducer =
+  () =>
+  (state = initialData, action) => {
+    switch (action.type) {
+      case TOGGLE_FAVOURITE:
+        let pokemon = action.payload;
+        let pokemonFromFavourite = state.favourites.find(
+          (favPokemon) => pokemon.id === favPokemon.id
+        );
+        return {
+          ...state,
+          favourites: pokemonFromFavourite
+            ? [
+                ...state.favourites.filter(
+                  (pokemon) => pokemon.id !== pokemonFromFavourite.id
+                ),
+              ]
+            : [...state.favourites, action.payload],
+        };
+      default:
+        return state;
+    }
+  };
 
-export default pokemonReducer
+export default pokemonReducer;
